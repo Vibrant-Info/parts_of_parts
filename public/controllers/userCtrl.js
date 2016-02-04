be2020c8b8d144bb4542d9d6cbc5a47c9e96cce9
vijaykamalr <vijaykamalr@gmail.com>
2016-02-04 16:18:12 +0530

    add-user

--- public/controllers/userCtrl.js
+++ public/controllers/userCtrl.js
@@ -2,14 +2,16 @@ app.controller('userCtrl',['$scope','$http','$timeout',function($scope,$http,$ti
 	$scope.saveUser=function(){
 		if($scope.repwd == $scope.user.usr_pwd){
 			$http.post('/addUser',$scope.user).success(function(response){
-				console.log(response);
 				$scope.successmsgshow=true;
 				$timeout(function(){
 					$scope.successmsgshow=false;
 				},3000);
 				$scope.user=[];
 			}).error(function(err){
-				console.log(err);
+				$scope.errmsgshow=true;
+				$timeout(function(){
+					$scope.errmsgshow=false;
+				},3000);
 			})
 		}
 		else{
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,11 @@ module.exports=function(app,connection){
 	app.post('/addUser',function(req,res){
 		var users=req.body;
 		var usr_sql=connection.query("INSERT INTO `tbl_users` SET ?",[users],function(err,results){
-			console.log(usr_sql.sql);
 			if(err){
 				console.log(err);
 				res.send(err);
 			}
 			else{
-				console.log(results);
 				res.send("success");
 			}
 		})
